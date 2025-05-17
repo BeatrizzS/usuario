023f92d09affa6ef39bc028805a1149758fd6146
Beatriz <derie.bs@gmail.com>
2025-05-17 00:12:11 -0300

    adicionada classes de security

---
+++ src/main/java/com/beawata/usuario/infrastructure/entity/Endereco.java
@@ -0,0 +1,23 @@
+package com.beawata.usuario.infrastructure.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "endereco")
+public class Endereco {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "rua")
+    private String rua;
+
+}
---
+++ src/main/java/com/beawata/usuario/infrastructure/repository/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package com.beawata.usuario.infrastructure.repository;
+
+import com.beawata.usuario.infrastructure.entity.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+}
